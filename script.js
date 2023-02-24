79bc6bef8d1d2c6aabf8ecc807395080fd6f2438
$TXRM <44237108+stxrm-cc@users.noreply.github.com>
2023-02-24 11:15:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 var textElement = document.querySelector(".content");
 
-var client_date = new Date().toLocaleTimeString();
-function time_of_day(date) {
-  // Function that checks if it's morning/afternoon/evening/night for the webpage visitor and returns that value
-  if (date.slice(-2) == "A" && 11 >= parseInt(date.slice(0,2)) >= 5){
-      return "morning";
-  } else if (date.slice(-2) == "P" && 5 >= parseInt(date.slice(0,2)) >= 12) {
-      return "afternoon";
-  } else if (date.slice(-2) == "P" && 9 >= parseInt(date.slice(0,2)) > 5) {
-      return "evening";
-  } else {
-      return "night";
-  }
-};
-
 var platforms = ["BGX", "LeagueSense"];
 var textContent = (`∞ Detour Services ∞\n\n\nWelcome to the Detour Bypass Official Gateway.\n\n\nWe are here to provide you with services to patch Riot Packman AntiCheat checks for your cheap, public platforms of choice.........\n\n\nThis is the only official website of Detour.\n\n\nThe current supported platforms are: ${platforms[0]}, ${platforms[1]}.........\n\n\nFor further communication needs you may contact me using my discord handle which is the following: sauce#9109.........\n\n\nHave a pleasant rest of your ${time_of_day(client_date)}......... \n\n\n\n\n\n\n\n\n\n\nsudo rm -rf logs.txt && sudo reboot  \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n`).split("");
 var frameIndex = 0;
 var drawIndex = 0;
+
+var client_date = new Date().toLocaleTimeString();
+var time = (date) => {
+    let parsed1 = parseInt(date.slice(0,1)), parsed2 = parseInt(date.slice(0,2)) ;
+    if (parsed1 == parsed2) {
+        return parsed1;
+    } else if (parsed1 < parsed2){
+        return parsed2;
+    }
+};
+
+function time_of_day(client, date) {
+  // Function that checks if it's morning/afternoon/evening/night for the webpage visitor and returns that value
+  client_ = client.slice(-2);
+  
+  // 5 <= date <= 11 doesn't work in JS (as it is supposed to, at least)
+  if (client_ == "AM" && (date >= 5 && date <= 11)){
+      return "morning";
+  } else if (client_ == "PM" && ((date >= 1 && date < 5) || date == 12)) {
+      return "afternoon";
+  } else if (client_ == "PM" && (date >= 5 && date < 9)) {
+      return "evening";
+  }
+  return "night";
+};
+
 
 function draw(time) {
   window.requestAnimationFrame(draw);
